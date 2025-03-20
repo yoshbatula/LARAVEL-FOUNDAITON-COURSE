c16a5a1237a3d5325c0bcfe5e712ec2f5e0b90db
yoshbatula <yoshbatula2@gmail.com>
2025-03-20 15:22:44 +0800

    progress

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,4 @@
 <x-layout>
     <h1 class="text-green-500">ABOUT PAGE</h1>
 
-    <form action="">
-        <label for="">yosh</label>
-        <input type="submit" value="Submit">
-    </form>
 </x-layout>
