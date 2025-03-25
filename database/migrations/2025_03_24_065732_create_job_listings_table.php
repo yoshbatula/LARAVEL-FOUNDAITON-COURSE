ac7f0a81f81046b09a4df838af8a92a27c2a52d2
yoshbatula <yoshbatula2@gmail.com>
2025-03-25 15:11:42 +0800

    progress

--- database/migrations/2025_03_24_065732_create_job_listings_table.php
+++ database/migrations/2025_03_24_065732_create_job_listings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title'); 
             $table->string('company');
             $table->string('salary');
             $table->timestamps();
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -109,6 +109,9 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         {{ $slot }}
     </div>
+    </div>
+
+
   </main>
 </div>
 
