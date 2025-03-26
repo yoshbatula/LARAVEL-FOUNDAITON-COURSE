1eaf87043d89d36c0646724c1e60db1d312866da
yoshbatula <yoshbatula2@gmail.com>
2025-03-26 15:14:35 +0800

    progress for models

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -21,6 +21,7 @@
               <x-nav-link  href="/" active="{{ request()->is('/') }}">Home</x-nav-link>
               <x-nav-link  href="/jobs" active="{{ request()->is('jobs') }}">Jobs</x-nav-link>
               <x-nav-link  href="/contact" active="{{ request()->is('contact') }}">Contact</x-nav-link>
+              <x-nav-link href="/donna" active="{{ request()->is('donna') }}">Donna</x-nav-link>
             </div>
           </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,13 @@ Route::get('/', function () {
     return view('home');
 });
 
-
 Route::get('/jobs', function () {
     return view('jobs', ['jobs' => Jobs::all()]);
         
 });
-
+Route::get('/donna', function () {
+    return view('donna');
+});
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Jobs::find($id);
